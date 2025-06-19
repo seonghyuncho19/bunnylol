e8b28e59684a0d53e0f95495225f9392438ef0e1
seonghyuncho19 <seonghyuncho19@users.noreply.github.com>
2025-06-19 15:58:32 +0000

    deploy: 5d47b13faa71cc963d769a24e78db3cdb1e84392

--- src/help.js
+++ src/help.js
@@ -1,10 +1,8 @@
 // @flow strict
 
 import type {CommandNames, CommandDataTableType, ColumnDataTableType} from './commands.js';
-import type {ClassCommands, JoinOrDiscussType, ClassType} from './classes.js';
-
 import {COMMANDS} from './commands.js';
-import {CLASSES} from './classes.js';
+
 
 export const viewHelpPage: () => void = function(){
     const data: Array<CommandDataTableType> = Object.keys(COMMANDS).map((command: CommandNames) => {
@@ -24,29 +22,6 @@ export const viewHelpPage: () => void = function(){
     $('#help-table').DataTable({
         data: data,
         columns: columns,
-        order: [[ 1, "asc" ]],
-        paging: false
-    });
-
-    const classesData: Array<CommandDataTableType> = Object.keys(CLASSES).map((command: ClassCommands) => {
-        const cmdData = CLASSES[command];
-        return {
-            name: cmdData.name, 
-            url: cmdData.url, 
-            // $FlowFixMe - this is actually correct.
-            command
-        };
-    });
-
-    const classColumns: Array<ColumnDataTableType> = [
-        {data: 'command', title: "Command"}, 
-        {data: 'name', title: "Name"}, 
-        {data: 'url', title: "URL"}, 
-    ];
-    // $FlowFixMe - jQuery import
-    $('#classes-table').DataTable({
-        data: classesData,
-        columns: classColumns,
         order: [[ 1, "asc" ]],
         paging: false
     });
